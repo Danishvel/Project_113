fed2808a818f751e1e0c64ba0c4515df2e8d3a78
Danishvel <79510005+Danishvel@users.noreply.github.com>
2021-07-18 10:59:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,20 +3,27 @@ function preload(){
 }
 
 function setup(){
-    canvas = createCanvas(400, 350);
-    canvas.position(110, 270);
+    canvas = createCanvas(400, 400);
+    canvas.position(110, 200);
     video = createCapture(VIDEO);
     video.hide();
-    tint_color = "";
 }
 
 function draw(){
-    image(video, 0, 0, 400, 350);
-    tint(tint_color);
-}
+    image(video, 50, 50, 300, 300);
 
-function Filter_Color(){
-    tint_color = document.getElementById("Filter color").value;
+fill(50, 168, 82);
+  stroke(50, 168, 82);
+  circle(35, 35, 70, 70);
+  circle(365, 37, 70, 70);
+  circle(35, 365, 70, 70);
+  circle(365, 365, 70, 70);
+  fill(214, 81, 81);
+  stroke(214, 81, 81);
+  rect(65, 15, 270, 40);
+  rect(15, 65, 40, 270);
+  rect(65, 345, 270, 40);
+  rect(345, 65, 40, 270);
 }
 
 function take_snapshot(){
